f02135d46d5df20cea5c02093aa34e3db3921d22
Michelle Primiani <primianimichelle@gmail.com>
2023-10-20 14:13:13 -0400

    POST Capabilities for dock view

--- json-server.py
+++ json-server.py
@@ -29,17 +29,17 @@ class JSONServer(HandleRequests):
 
     def do_POST(self):
         # Parse the URL
-
+        url = self.parse_url(self.path)
         # Determine the correct view needed to handle the requests
-
+        view = self.determine_view(url)
         # Get the request body
-
+        request_body = self.get_request_body()
         # Invoke the correct method on the view
-
+        try:
+            view.create(self, request_body)
         # Make sure you handle the AttributeError in case the client requested a route that you don't support
-
-        # Once you implement this method, delete the following line of code
-        return self.response("", status.HTTP_405_UNSUPPORTED_METHOD.value)
+        except AttributeError:
+            return self.response("No view for that route", status.HTTP_404_CLIENT_ERROR_RESOURCE_NOT_FOUND.value)
 
     def do_DELETE(self):
         url = self.parse_url(self.path)
@@ -49,17 +49,6 @@ class JSONServer(HandleRequests):
             view.delete(self, url["pk"])
         except AttributeError:
             return self.response("No view for that route", status.HTTP_404_CLIENT_ERROR_RESOURCE_NOT_FOUND.value)
-
-
-
-
-
-
-
-
-
-
-
 
     def determine_view(self, url):
         """Lookup the correct view class to handle the requested route
@@ -83,12 +72,6 @@ class JSONServer(HandleRequests):
             return status.HTTP_404_CLIENT_ERROR_RESOURCE_NOT_FOUND.value
 
 
-
-
-
-
-
-
 #
 # THE CODE BELOW THIS LINE IS NOT IMPORTANT FOR REACHING YOUR LEARNING OBJECTIVES
 #
@@ -96,6 +79,7 @@ def main():
     host = ''
     port = 8000
     HTTPServer((host, port), JSONServer).serve_forever()
+
 
 if __name__ == "__main__":
     main()
